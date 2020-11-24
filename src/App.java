530ca29021c0111c1a18dbb08a380f9ec9fff703
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2020-11-23 23:37:56 -0800

    removeVertex not fixed

--- src/App.java
+++ src/App.java
@@ -1,13 +1,13 @@
 import java.util.*;
 public class App {
     public static void main(String[] args){
-        //cls.clearScreen();
+        cls.clearScreen();
 
         ArrayList<String> vertices = new ArrayList<>();
 
         String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
         ArrayList<String> inside = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i <15; i++) {
             Random rnd = new Random();
             String s = String.valueOf(ALPHABET.charAt(rnd.nextInt(ALPHABET.length())));    
             
@@ -21,20 +21,32 @@ public class App {
         //INICIALIZAMOS EL ARRAY DESPUES DE CREAR LA LISTA DE VERTICES
         Graph graph = new Graph(vertices);
 
-        for (int i = 0; i < 50; i++) {
         int SIZE = inside.size();
+        for (int i = 0; i<10; i++) {
         Random rndStart = new Random();
         Random rndEnd = new Random();
         String start = inside.get(rndStart.nextInt(SIZE));
         String end = inside.get(rndEnd.nextInt(SIZE));
-            
-        graph.addEdge(start, end);
-        graph.counter++;
+        //para evitar loop en uno mismo
+        if(!start.equals(end)) {
+            graph.addEdge(start, end);
+            graph.counter++;
+        }
+        
+        else{
+            i--;
+        }
+
     }
     
 
     graph.printGraph();
     graph.DFS();
+    //graph.removeVertex("p");
+    //System.out.println("\n-----\n");
+    //graph.DFS();
+    System.out.println("\n-----\n");
+    //graph.printGraph();
     System.out.println("\nN edges:"+String.valueOf(graph.counter));
     }
 }
